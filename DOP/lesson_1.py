a2f0de98c19a8c8ae33068e9d45451cd0d45e535
Deepak HB <deepakhb2@gmail.com>
2019-03-10 18:15:36 +0530

    Commiting missed files in my previous commit

---
+++ DOP/lesson_1.py
@@ -0,0 +1,15 @@
+class Student:
+
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+
+kitty = Student('Kitty', 85, 30)
+daniel = Student('Daniel', 80, 15)
+
+print(kitty.name)
+print(daniel.name)
+
+print(kitty.age)
+print(daniel.age)
